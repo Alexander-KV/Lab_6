6d51863591e64eddab0d566b51ec3a8754ed516a
Александр <total_elit@bk.ru>
2026-05-16 22:27:43 +0300

    Добавьте файлы проекта.

---
+++ CalculatorApp.Tests/CalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ReturnsCorrectSum()
+        {
+            Calculator calc = new Calculator();
+            int result = calc.add(2, 3);
+            Assert.Equal(5, result);
+        }
+    }
+}
